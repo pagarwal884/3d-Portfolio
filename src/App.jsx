ad2726463864251e110017ac8b43f521aa9b4f72
pagarwal884 <pagarwal1145@gmail.com>
2025-07-09 14:11:31 +0530

    Working on Navbar and also added few more things from my side to make this code mine

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+
 import {
   About,
   Contact,
@@ -10,26 +11,27 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
-const App = () => {
-  return;
-  <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <Hero />
-      </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
 
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-    </div>
-  </BrowserRouter>;
+    </BrowserRouter>
+  );
 };
 
 export default App;
