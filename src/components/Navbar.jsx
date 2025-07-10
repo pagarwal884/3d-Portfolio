3257dce7b0fef6ded2a56798af9850fb870aab57
pagarwal884 <pagarwal1145@gmail.com>
2025-07-10 16:00:48 +0530

    Done the Navbar and Also add my own designed logo in the navbar and started creating Hero section in which i have make it more Beautiful and also try to make it professional as well as modern

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { menu, close, logo } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav
@@ -18,14 +19,14 @@ const Navbar = () => {
             setActive("");
             window.scrollTo(0, 0);
           }}>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Piyush{" "}
+          <img src={logo} alt="Logo" className="w-12 h-12 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Piyush&nbsp;
             <span className="sm:block hidden">| Full Stack Developer</span>
           </p>
         </Link>
 
-        {/* Navbar Links */}
+        {/* Desktop Navigation */}
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((nav) => (
             <li
@@ -38,8 +39,37 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <div className="sm:hidden flex flex-1 justify-end items-center">
 
+        {/* Mobile Navigation */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="Menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+            aria-label={toggle ? "Close menu" : "Open menu"}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end items-center flex-col gap-4">
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(false);
+                    setActive(nav.title);
+                  }}>
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
